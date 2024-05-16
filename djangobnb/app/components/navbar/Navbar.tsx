3068821d9da27f11bfc1235c2f1a92a1088dbea0
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-15 23:35:44 -0700

    Update navbar

--- djangobnb/app/components/navbar/Navbar.tsx
+++ djangobnb/app/components/navbar/Navbar.tsx
@@ -1,9 +1,30 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-      Navbar
+    <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+      <div className="max-w-[1500px] mx-auto px-6">
+        <div className="flex justify-between items-center">
+          <Link href="/">
+            <Image
+              src="/logo.png"
+              alt="DjangoBnb logo"
+              width={180}
+              height={38}
+            />
+          </Link>
+
+          <div className="flex space-x-6">
+            search filters
+          </div>
+
+          <div className="flex items-center space-x-6">
+            add property - user nav
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
