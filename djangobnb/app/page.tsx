4b47fa76443daa3661a9d7811e0255bfb2bc0101
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-20 20:57:43 -0700

    Add PropertyList component

--- djangobnb/app/page.tsx
+++ djangobnb/app/page.tsx
@@ -1,9 +1,11 @@
 import Categories from "./components/Categories";
+import PropertyList from "./components/properties/PropertyList";
 
 export default function Home() {
   return (
     <main className="max-w-[1500px] mx-auto px-6">
       <Categories />
+      <PropertyList />
     </main>
   );
 }
