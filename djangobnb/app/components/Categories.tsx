0cf52c62240c2e9e6c64faf54bc759ada125c33a
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-20 20:55:50 -0700

    Update categories

--- djangobnb/app/components/Categories.tsx
+++ djangobnb/app/components/Categories.tsx
@@ -12,6 +12,36 @@ const Categories = () => {
         />
         <span className='text-xs'>Beach</span>
       </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
+        <span className='text-xs'>Villas</span>
+      </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
+        <span className='text-xs'>Cabin</span>
+      </div>
+
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
+        <span className='text-xs'>Tiny Homes</span>
+      </div>
     </div>
   )
 }
