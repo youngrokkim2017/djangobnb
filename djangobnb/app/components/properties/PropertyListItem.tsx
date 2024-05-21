843d606f347731ae8fa3059d04d9cab68c9735ea
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-21 00:04:24 -0700

    Update property list item

--- djangobnb/app/components/properties/PropertyListItem.tsx
+++ djangobnb/app/components/properties/PropertyListItem.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
 
 const PropertyListItem = () => {
   return (
-    <div>PropertyListItem</div>
+    <div className="cursor-pointer">
+      <div className="relative overflow-hidden aspect-square rounded-xl">
+        <Image
+          fill
+          src="/beach_1.jpg"
+          sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
+          className="hover:scale-110 object-cover transition h-full w-full"
+          alt="Beach house"
+        />
+      </div>
+      <div className="mt-2">
+        <p className="text-lg font-bold">Property Name</p>
+      </div>
+      <div className="mt-2">
+        <p className="text-sm text-gray-500"><strong>$Property Price$</strong> per night</p>
+      </div>
+    </div>
   )
 }
 
