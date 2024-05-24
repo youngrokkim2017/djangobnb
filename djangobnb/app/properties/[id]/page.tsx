19712b4595feb8c932ead302429e0578a2a91144
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-23 22:53:08 -0700

    Add left side

--- djangobnb/app/properties/[id]/page.tsx
+++ djangobnb/app/properties/[id]/page.tsx
@@ -14,7 +14,31 @@ const PropertDetailPage = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
         <div className="py-6 pr-6 col-span-3">
-          left
+          <h1 className="mb-4 text-4xl">property name</h1>
+          
+          <span className="mb-6 block text-lg text-gray-600">
+            # guests - # bedrooms - # bathrooms
+          </span>
+
+          <hr />
+
+          <div className="py-6 flex items-center space-x-4">
+            <Image
+              src="/profile_pic_1.jpg"
+              width={50}
+              height={50}
+              className="rouned-full"
+              alt="user name"
+            />
+
+            <p><strong>John Doe</strong> is your host</p>
+          </div>
+
+          <hr />
+           
+          <p className="mt-6 text-lg">
+            property description
+          </p>
         </div>
 
         <div>
