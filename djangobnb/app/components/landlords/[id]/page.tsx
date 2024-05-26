88cfed0bbdb01509d65ebf02ed4970bf426ee9e0
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-25 23:36:55 -0700

    Add landlord left side

--- djangobnb/app/components/landlords/[id]/page.tsx
+++ djangobnb/app/components/landlords/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const LandlordDetailPage = () => {
@@ -5,7 +6,18 @@ const LandlordDetailPage = () => {
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         <aside className="col-span-1 mb-4">
-          left
+          <div className="flex flex-col items-center p-6 rounded-xl border border-gray-300 shadow-xl">
+            <Image
+              src="/profile_pic_1.jpg"
+              width={200}
+              height={200}
+              alt="Landlord name"
+              className="rounded-full"
+            />
+            <h1 className="mt-6 text-2xl">landlord name</h1>
+
+            Contact Button
+          </div>
         </aside>
         
         <div className="col-span-1 md:col-span-3 pl-0 md:pl-6">
