f91f986c49d145564d6d1576f9444f7634baa19b
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-29 14:47:42 -0700

    Add inbox component

---
+++ djangobnb/app/components/inbox/Conversation.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Conversation = () => {
+  return (
+    <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
+      <p className="mb-6 text-xl">
+        John Doe
+      </p>
+      <p className="text-airbnb-dark">
+        Go to Conversations
+      </p>
+    </div>
+  )
+}
+
+export default Conversation
