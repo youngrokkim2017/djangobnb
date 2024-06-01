718ccb3d2784bdfadf0ac6557403f5a81a9b93ea
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-31 23:38:22 -0700

    Update styling

--- djangobnb/app/components/inbox/ConversationDetail.tsx
+++ djangobnb/app/components/inbox/ConversationDetail.tsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const ConversationDetail = () => {
   return (
-    <div>ConversationDetail</div>
+    <>
+      <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
+        <div className="w-[80%]py-4 px-6 rounded-xl bg-gray-200">
+          <p className="font-bold text-gray-500">
+            message
+          </p>
+        </div>
+  
+        <div className="w-[80%] ml-[20%] py-4 px-6 rounded-xl bg-blue-200">
+          <p className="font-bold text-gray-500">
+            name
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-4 py-4 px-6 flex border border-gray-300 space-x-4 rounded-xl">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          className="w-full p-2 bg-gray-200 rounded-xl"
+        />
+
+        <button></button>
+      </div>
+    </>
   )
 }
 
