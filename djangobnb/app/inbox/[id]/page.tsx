861e45f125cf3ef8fc3b4511131888bbafec8885
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-31 23:30:32 -0700

    Add Conversation Page

---
+++ djangobnb/app/inbox/[id]/page.tsx
@@ -0,0 +1,9 @@
+const ConversationPage = () => {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6">
+      Conversation Page
+    </main>
+  )
+}
+
+export default ConversationPage
