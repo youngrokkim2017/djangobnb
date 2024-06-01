ed6ac7453f8b944f4cbb9b8d8a6dc189946f4395
Patrick Kim <patrick.yrkim@gmail.com>
2024-05-31 23:31:27 -0700

    Add ConversationDetail component

---
+++ djangobnb/app/components/inbox/ConversationDetail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConversationDetail = () => {
+  return (
+    <div>ConversationDetail</div>
+  )
+}
+
+export default ConversationDetail
--- djangobnb/app/inbox/[id]/page.tsx
+++ djangobnb/app/inbox/[id]/page.tsx
@@ -1,7 +1,9 @@
+import ConversationDetail from "@/app/components/inbox/ConversationDetail"
+
 const ConversationPage = () => {
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
-      Conversation Page
+      <ConversationDetail />
     </main>
   )
 }
