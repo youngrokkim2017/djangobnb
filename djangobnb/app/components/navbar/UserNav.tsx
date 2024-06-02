ddaa52444a6cc4f542146f501ae8ee198b97794a
Patrick Kim <patrick.yrkim@gmail.com>
2024-06-01 23:30:48 -0700

    Add MenuLink component

--- djangobnb/app/components/navbar/UserNav.tsx
+++ djangobnb/app/components/navbar/UserNav.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+import MenuLink from './MenuLink'
 
 const UserNav = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className="p-2 relative inline-block border rounded-full">
       <button className="flex items-center">
@@ -11,6 +16,19 @@ const UserNav = () => {
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
         </svg>
       </button>
+
+      {isOpen && (
+        <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
+          <MenuLink 
+            label='Log in'
+            onClick={() => console.log('clicked button')} 
+          />
+          <MenuLink 
+            label='Sign up' 
+            onClick={() => console.log('clicked button')}
+          />
+        </div>
+      )}
     </div>
   )
 }
