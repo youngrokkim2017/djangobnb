3214faeadf4a3a6b0b0569e78e688f30963608a4
Patrick Kim <patrick.yrkim@gmail.com>
2024-06-03 17:01:19 -0700

    Add isOpen useState

--- djangobnb/app/components/navbar/UserNav.tsx
+++ djangobnb/app/components/navbar/UserNav.tsx
@@ -8,7 +8,10 @@ const UserNav = () => {
 
   return (
     <div className="p-2 relative inline-block border rounded-full">
-      <button className="flex items-center">
+      <button 
+        className="flex items-center"
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
