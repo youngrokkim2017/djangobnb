68a92c031e7b29d339a4a7435ceb97a13106b368
Patrick Kim <patrick.yrkim@gmail.com>
2024-06-06 00:59:18 -0700

    Update login modal content

--- djangobnb/app/components/modals/LoginModal.tsx
+++ djangobnb/app/components/modals/LoginModal.tsx
@@ -4,14 +4,27 @@ import React from 'react'
 
 import Modal from './Modal'
 import useLoginModal from '@/app/hooks/useLoginModal'
+import CustomButton from '../forms/CustomButton'
 
 const LoginModal = () => {
   const loginModal = useLoginModal()
 
   const content = (
-    <h2 className="mb-6 text-2xl">
-      Welcome to Djangobnb, please log in
-    </h2>
+    <>
+      <form>
+        <input placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+        <input placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+      </form>
+
+      <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
+        error message
+      </div>
+
+      <CustomButton
+        label="Submit"
+        onClick={() => console.log('test')}
+      />
+    </>
   )
 
   return (
