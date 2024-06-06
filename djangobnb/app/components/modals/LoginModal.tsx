fb9c00fcb54b0eefb662a73826cf81dd1442c208
Patrick Kim <patrick.yrkim@gmail.com>
2024-06-06 00:49:04 -0700

    Add modal template

--- djangobnb/app/components/modals/LoginModal.tsx
+++ djangobnb/app/components/modals/LoginModal.tsx
@@ -1,8 +1,26 @@
+'use client'
+
 import React from 'react'
 
+import Modal from './Modal'
+import useLoginModal from '@/app/hooks/useLoginModal'
+
 const LoginModal = () => {
+  const loginModal = useLoginModal()
+
+  const content = (
+    <h2 className="mb-6 text-2xl">
+      Welcome to Djangobnb, please log in
+    </h2>
+  )
+
   return (
-    <div>LoginModal</div>
+    <Modal
+      isOpen={loginModal.isOpen}
+      close={loginModal.close}
+      label="Log in"
+      content={content}
+    />
   )
 }
 
