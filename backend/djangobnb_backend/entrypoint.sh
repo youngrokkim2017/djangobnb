5bc83ef74e554d9b8424bcc4ea801180f8872e3a
Patrick Kim <patrick.yrkim@gmail.com>
2024-06-16 15:33:55 -0700

    Add entrypoint for docker setup

---
+++ backend/djangobnb_backend/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ] 
+then
+    echo "Check if database is running..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "The database is up and running :-D"
+fi
+
+python manage.py migrate
+
+exec "$@"
