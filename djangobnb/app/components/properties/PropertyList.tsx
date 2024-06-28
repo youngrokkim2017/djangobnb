89bc9aa1a9c86aa70717d11a75aa09ac5de4826b
Patrick Kim <patrick.yrkim@gmail.com>
2024-06-27 23:54:34 -0700

    Update dynamic property list

--- djangobnb/app/components/properties/PropertyList.tsx
+++ djangobnb/app/components/properties/PropertyList.tsx
@@ -1,12 +1,47 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import PropertyListItem from './PropertyListItem'
 
+export type PropertyType = {
+  id: string;
+  title: string;
+  price_per_night: number;
+  image_url: string;
+}
+
 const PropertyList = () => {
+  const [properties, setProperties] = useState<PropertyType[]>([])
+
+  const getProperties = async () => {
+    const url = 'http://localhost:8000/api/properties'
+
+    await fetch(url, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then((json) => {
+        console.log('json', json)
+      })
+      .catch((error) => {
+        console.log('error', error)
+      })
+  }
+  
+  useEffect(() => {
+    getProperties()
+  }, [])
+
   return (
     <>
-      <PropertyListItem />
-      <PropertyListItem />
-      <PropertyListItem />
+      {properties.map((property) => {
+        return (
+          <PropertyListItem 
+            key={property.id}
+            property={property}
+          />
+        )
+      })}
     </>
   )
 }
