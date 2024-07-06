6b9f7469c7d816711ab0dd58464a9fb09fccd52e
Patrick Kim <patrick.yrkim@gmail.com>
2024-07-06 00:03:20 -0700

    Update form

--- djangobnb/app/components/modals/SignupModal.tsx
+++ djangobnb/app/components/modals/SignupModal.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation'
 import Modal from './Modal'
 import useSignupModal from '@/app/hooks/useSignupModal'
 import CustomButton from '../forms/CustomButton'
+import apiService from '@/app/services/apiService'
 
 const SignupModal = () => {
   const signupModal = useSignupModal()
@@ -15,9 +16,36 @@ const SignupModal = () => {
   const [password2, setPassword2] = useState('')
   const [errors, setErrors] = useState<string[]>([])
 
+  const submitSignup = async () => {
+    const formData = {
+      email: email,
+      password1: password1,
+      password2: password2,
+    }
+
+    const response = await apiService.post('/api/auth/register', formData)
+
+    if (response.access) {
+      // handleLogin
+
+      signupModal.close()
+
+      router.push('/')
+    } else {
+      const tmpErrors: string[] = Object.values(response).map((error: any) => {
+        return error
+      })
+
+      setErrors(tmpErrors)
+    }
+  }
+
   const content = (
     <>
-      <form>
+      <form
+        action={submitSignup}
+        className='space-y-4'
+      >
         <input onChange={(e) => setEmail(e.target.value)} placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
         <input onChange={(e) => setPassword1(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
         <input onChange={(e) => setPassword2(e.target.value)} placeholder="Repeat password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
