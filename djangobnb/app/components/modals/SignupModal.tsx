ee318d0c88c84cd2e44ab7b6de09b9b2284e2890
Patrick Kim <patrick.yrkim@gmail.com>
2024-07-05 23:57:02 -0700

    Update signup modal states

--- djangobnb/app/components/modals/SignupModal.tsx
+++ djangobnb/app/components/modals/SignupModal.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 import Modal from './Modal'
 import useSignupModal from '@/app/hooks/useSignupModal'
@@ -8,18 +9,27 @@ import CustomButton from '../forms/CustomButton'
 
 const SignupModal = () => {
   const signupModal = useSignupModal()
+  const router = useRouter()
+  const [email, setEmail] = useState('')
+  const [password1, setPassword1] = useState('')
+  const [password2, setPassword2] = useState('')
+  const [errors, setErrors] = useState<string[]>([])
 
   const content = (
     <>
       <form>
-        <input placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
-        <input placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
-        <input placeholder="Repeat password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+        <input onChange={(e) => setEmail(e.target.value)} placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+        <input onChange={(e) => setPassword1(e.target.value)} placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+        <input onChange={(e) => setPassword2(e.target.value)} placeholder="Repeat password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
       </form>
 
-      <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-        error message
-      </div>
+      {errors.map((error, index) => {
+        return (
+          <div key={`error_${index}`} className="p-5 bg-airbnb text-white rounded-xl opacity-80">
+            {error}
+          </div>
+        )
+      })}
 
       <CustomButton
         label="Submit"
