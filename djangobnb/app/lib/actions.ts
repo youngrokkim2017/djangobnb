167023eee72bcc026115abbdc7e1f967056b2756
Patrick Kim <patrick.yrkim@gmail.com>
2024-07-11 23:17:17 -0700

    Add lib actions

---
+++ djangobnb/app/lib/actions.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import { cookies } from 'next/headers'
+
+export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
+    cookies().set('session_userid', userId, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/'
+    })
+
+    cookies().set('session_access_token', accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60,
+        path: '/'
+    })
+
+    cookies().set('session_refresh_token', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/'
+    })
+}
