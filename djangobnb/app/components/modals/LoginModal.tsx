b75221b408cf74848190712ee95f6e18ee7c40a0
Patrick Kim <patrick.yrkim@gmail.com>
2024-07-13 23:45:33 -0700

    Update login form

--- djangobnb/app/components/modals/LoginModal.tsx
+++ djangobnb/app/components/modals/LoginModal.tsx
@@ -1,29 +1,48 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 import Modal from './Modal'
 import useLoginModal from '@/app/hooks/useLoginModal'
 import CustomButton from '../forms/CustomButton'
+import apiService from '@/app/services/apiService'
+import { handleLogin } from '@/app/lib/actions'
 
 const LoginModal = () => {
   const loginModal = useLoginModal()
+  const router = useRouter()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState<string[]>([])
 
   const content = (
     <>
       <form>
-        <input placeholder="Your e-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
-        <input placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+        <input 
+          onChange={(e) => setEmail(e.target.value)}
+          type="email" 
+          placeholder="Your e-mail address" 
+          className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" 
+        />
+        <input 
+          type="password" 
+          placeholder="Your password" 
+          className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" 
+        />
+
+        {errors.map((error, index) => {
+          return (
+            <div key={`error_${index}`} className="p-5 bg-airbnb text-white rounded-xl opacity-80">
+              {error}
+            </div>
+          )
+        })}
+        <CustomButton
+          label="Submit"
+          onClick={() => console.log('test')}
+        />
       </form>
-
-      <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
-        error message
-      </div>
-
-      <CustomButton
-        label="Submit"
-        onClick={() => console.log('test')}
-      />
     </>
   )
 
