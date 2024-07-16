c249fe3b3eae5ce93a14f3e5a53da7a05dd1b08d
Patrick Kim <patrick.yrkim@gmail.com>
2024-07-15 23:47:27 -0700

    Update user id functionality

--- djangobnb/app/components/navbar/Navbar.tsx
+++ djangobnb/app/components/navbar/Navbar.tsx
@@ -4,8 +4,11 @@ import Link from 'next/link'
 import SearchFilters from './SearchFilters'
 import UserNav from './UserNav'
 import AddPropertyButton from './AddPropertyButton'
+import { getUserId } from '@/app/lib/actions'
 
-const Navbar = () => {
+const Navbar = async () => {
+  const userId = await getUserId()
+
   return (
     <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
       <div className="max-w-[1500px] mx-auto px-6">
@@ -26,7 +29,9 @@ const Navbar = () => {
           <div className="flex items-center space-x-6">
             <AddPropertyButton />
 
-            <UserNav />
+            <UserNav 
+              userId={userId}
+            />
           </div>
         </div>
       </div>
--- djangobnb/app/components/navbar/UserNav.tsx
+++ djangobnb/app/components/navbar/UserNav.tsx
@@ -6,7 +6,13 @@ import useLoginModal from '@/app/hooks/useLoginModal'
 import useSignupModal from '@/app/hooks/useSignupModal'
 import LogoutButton from '../LogoutButton'
 
-const UserNav = () => {
+interface UserNavProps {
+  userId?: string | null;
+}
+
+const UserNav: React.FC<UserNavProps> = ({
+  userId
+}) => {
   const [isOpen, setIsOpen] = useState(false)
   const loginModal = useLoginModal()
   const signupModal = useSignupModal()
@@ -27,21 +33,27 @@ const UserNav = () => {
 
       {isOpen && (
         <div className="w-[220px] absolute top-[60px] right-0 bg-white border rounded-xl shadow-md flex flex-col cursor-pointer">
-          <MenuLink 
-            label='Log in'
-            onClick={() => {
-              setIsOpen(false)
-              loginModal.open()
-            }} 
-          />
-          <MenuLink 
-            label='Sign up' 
-            onClick={() => {
-              setIsOpen(false)
-              signupModal.open()
-            }} 
-          />
-          <LogoutButton />
+          {userId ? (
+            <LogoutButton />
+          ) : (
+            <>
+              <MenuLink 
+                label='Log in'
+                onClick={() => {
+                  setIsOpen(false)
+                  loginModal.open()
+                }} 
+              />
+              <MenuLink 
+                label='Sign up' 
+                onClick={() => {
+                  setIsOpen(false)
+                  signupModal.open()
+                }} 
+              />
+            </>
+          )}
+          
         </div>
       )}
     </div>
