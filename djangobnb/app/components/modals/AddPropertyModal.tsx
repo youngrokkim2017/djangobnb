27be8d828e3184a05e65261ab9118f4177a9f830
Patrick Kim <patrick.yrkim@gmail.com>
2024-07-20 23:57:25 -0700

    Update steps 2 and 3

--- djangobnb/app/components/modals/AddPropertyModal.tsx
+++ djangobnb/app/components/modals/AddPropertyModal.tsx
@@ -15,7 +15,13 @@ const AddPropertyModal = () => {
     // states
     const [currentStep, setCurrentStep] = useState(1)
     const [dataCategory, setDataCategory] = useState('')
-    
+    const [dataTitle, setDataTitle] = useState('')
+    const [dataDescription, setDataDescription] = useState('')
+    const [dataPrice, setDataPrice] = useState('')
+    const [dataBedrooms, setDataBedrooms] = useState('')
+    const [dataBathrooms, setDataBathrooms] = useState('')
+    const [dataGuests, setDataGuests] = useState('')
+
     //  set data
     const setCategory = (category: string) => {
         setDataCategory(category)
@@ -37,8 +43,107 @@ const AddPropertyModal = () => {
                         onClick={() => setCurrentStep(2)}
                     />
                 </>
-            ): (
-                <p>step 2</p>
+            ) : currentStep == 2 ? (
+                <>
+                    <h2 className='mb-6 text-2xl'>Describe your place</h2>
+
+                    <div className='pt-3 pb-6 space-y-4'>
+                        <div className='flex flex-col space-y-2'>
+                            <label>Title</label>
+                            <input
+                                type="text"
+                                value={dataTitle}
+                                onChange={(e) => setDataTitle(e.target.value)}
+                                className='w-full p-4 border border-gray-600 rounded-xl'
+                            />
+                        </div>
+
+                        <div className='flex flex-col space-y-2'>
+                            <label>Description</label>
+                            <textarea
+                                value={dataDescription}
+                                onChange={(e) => setDataDescription(e.target.value)}
+                                className='w-full h-[200px] p-4 border border-gray-600 rounded-xl'
+                            ></textarea>
+                        </div>
+                    </div>
+
+                    <CustomButton
+                        label='Previous'
+                        className='mb-2 bg-black hover:bg-gray-800'
+                        onClick={() => setCurrentStep(1)}
+                    />
+
+                    <CustomButton
+                        label='Next'
+                        onClick={() => setCurrentStep(3)}
+                    />
+                </>
+            ) : currentStep == 3 ? (
+                <>
+                    <h2 className='mb-6 text-2xl'>Details</h2>
+
+                    <div className='pt-3 pb-6 space-y-4'>
+                        <div className='flex flex-col space-y-2'>
+                            <label>Price per night</label>
+                            <input
+                                type="number"
+                                value={dataPrice}
+                                onChange={(e) => setDataPrice(e.target.value)}
+                                className='w-full p-4 border border-gray-600 rounded-xl'
+                            />
+                        </div>
+
+                        <div className='flex flex-col space-y-2'>
+                            <label>Bedrooms</label>
+                            <input
+                                type="number"
+                                value={dataBedrooms}
+                                onChange={(e) => setDataBedrooms(e.target.value)}
+                                className='w-full p-4 border border-gray-600 rounded-xl'
+                            />
+                        </div>
+
+                        <div className='flex flex-col space-y-2'>
+                            <label>Bathrooms</label>
+                            <input
+                                type="number"
+                                value={dataBathrooms}
+                                onChange={(e) => setDataBathrooms(e.target.value)}
+                                className='w-full p-4 border border-gray-600 rounded-xl'
+                            />
+                        </div>
+
+                        <div className='flex flex-col space-y-2'>
+                            <label>Maximum number of guests</label>
+                            <input
+                                type="number"
+                                value={dataGuests}
+                                onChange={(e) => setDataGuests(e.target.value)}
+                                className='w-full p-4 border border-gray-600 rounded-xl'
+                            />
+                        </div>
+                    </div>
+
+                    <CustomButton
+                        label='Previous'
+                        className='mb-2 bg-black hover:bg-gray-800'
+                        onClick={() => setCurrentStep(2)}
+                    />
+
+                    <CustomButton
+                        label='Next'
+                        onClick={() => setCurrentStep(4)}
+                    />
+                </>
+            ) : currentStep == 4 ? (
+                <>
+
+                </>
+            ) : (
+                <>
+                    
+                </>
             )}
         </>
     )
