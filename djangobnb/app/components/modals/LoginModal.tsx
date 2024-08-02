3b0011151e9f30850569c1070c7db82004eb1ea6
Patrick Kim <patrick.yrkim@gmail.com>
2024-08-01 18:48:28 -0700

    Update post with token and post without token

--- djangobnb/app/components/modals/LoginModal.tsx
+++ djangobnb/app/components/modals/LoginModal.tsx
@@ -22,7 +22,7 @@ const LoginModal = () => {
       password: password
     }
 
-    const response = await apiService.post('/api/auth/register', JSON.stringify(formData))
+    const response = await apiService.postWithoutToken('/api/auth/register', JSON.stringify(formData))
 
     if (response.access) {
       handleLogin(response.user.pk, response.access, response.refresh);
