bf6f3de1824283a0263cf2ecfe81a15081469faf
Patrick Kim <patrick.yrkim@gmail.com>
2024-08-04 15:58:30 -0700

    Add PropertiesDetailSerializer

--- backend/djangobnb_backend/property/serializers.py
+++ backend/djangobnb_backend/property/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from .models import Property
 
+from useraccount.serializers import UserDetailSerializer
+
 class PropertiesListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
@@ -10,4 +12,21 @@ class PropertiesListSerializer(serializers.ModelSerializer):
             'title',
             'price_per_night',
             'image_url',
+        )
+
+class PropertiesDetailSerializer(serializers.ModelSerializer):
+    landlord = UserDetailSerializer(read_only=True, many=False)
+
+    class Meta:
+        model = Property
+        fields = (
+            'id',
+            'title',
+            'description',
+            'price_per_night',
+            'image_url',
+            'bedrooms',
+            'bathrooms',
+            'guests',
+            'landlord'
         )
