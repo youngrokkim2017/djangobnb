c42440e266c140ce94af3310358b827792b66b81
Patrick Kim <patrick.yrkim@gmail.com>
2024-08-11 14:23:22 -0700

    Add router

--- djangobnb/app/components/properties/PropertyListItem.tsx
+++ djangobnb/app/components/properties/PropertyListItem.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 import { PropertyType } from './PropertyList'
 
@@ -10,8 +11,13 @@ interface PropertyProps {
 const PropertyListItem: React.FC<PropertyProps> = ({
   property
 }) => {
+  const router = useRouter()
+
   return (
-    <div className="cursor-pointer">
+    <div 
+      className="cursor-pointer"
+      onClick={() => router.push(`/properties/${property.id}`)}
+    >
       <div className="relative overflow-hidden aspect-square rounded-xl">
         <Image
           fill
