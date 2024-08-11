17ffcd5495910d28f2fb9528a90edc1887defab3
Patrick Kim <patrick.yrkim@gmail.com>
2024-08-10 23:56:20 -0700

    Add properties_detail api

--- backend/djangobnb_backend/property/api.py
+++ backend/djangobnb_backend/property/api.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
 from .models import Property
-from .serializers import PropertiesListSerializer
+from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
 from .forms import PropertyForm
 
 @api_view(['GET'])
@@ -16,6 +16,16 @@ def properties_list(request):
     return JsonResponse({
         'data': serializer.data
     })
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def properties_detail(request, pk):
+    property = Property.objects.get(pk=pk)
+
+    serializer = PropertiesDetailSerializer(property, many=False)
+
+    return JsonResponse(serializer.data)
 
 @api_view(['POST', 'FILES'])
 def create_property(request):
