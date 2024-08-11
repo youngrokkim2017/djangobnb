aff348cf2f91e87bfb3780a09617d317f691cce8
Patrick Kim <patrick.yrkim@gmail.com>
2024-08-11 14:33:35 -0700

    Update property info with dynamic data

--- djangobnb/app/properties/[id]/page.tsx
+++ djangobnb/app/properties/[id]/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
+import apiService from "@/app/services/apiService";
 
-const PropertDetailPage = () => {
+const PropertDetailPage = async ({ params }: { params: { id: string }}) => {
+  const property = await apiService.get(`/api/properties/${params.id}`)
+
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
       <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
@@ -16,30 +19,32 @@ const PropertDetailPage = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
         <div className="py-6 pr-6 col-span-3">
-          <h1 className="mb-4 text-4xl">property name</h1>
+          <h1 className="mb-4 text-4xl">{property.title}</h1>
           
           <span className="mb-6 block text-lg text-gray-600">
-            # guests - # bedrooms - # bathrooms
+          {property.guests} guests - {property.bedrooms} bedrooms - {property.bathrooms} bathrooms
           </span>
 
           <hr />
 
           <div className="py-6 flex items-center space-x-4">
+          {property.landlord.avatar_url && (
             <Image
-              src="/profile_pic_1.jpg"
+              src={property.landlord.avatar_url}
               width={50}
               height={50}
               className="rouned-full"
               alt="user name"
             />
+          )}
 
-            <p><strong>John Doe</strong> is your host</p>
+            <p><strong>{property.landlord.name}</strong> is your host</p>
           </div>
 
           <hr />
            
           <p className="mt-6 text-lg">
-            property description
+          {property.description}
           </p>
         </div>
 
