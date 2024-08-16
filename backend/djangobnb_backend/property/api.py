5a6b0e63f69f4e66c9806b4b59ca11f283726095
Patrick Kim <patrick.yrkim@gmail.com>
2024-08-15 23:44:50 -0700

    Add book property api

--- backend/djangobnb_backend/property/api.py
+++ backend/djangobnb_backend/property/api.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
-from .models import Property
+from .models import Property, Reservation
 from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
 from .forms import PropertyForm
 
@@ -40,3 +40,30 @@ def create_property(request):
     else:
         print('error', form.errors, form.non_field_errors)
         return JsonResponse({'errors': form.errors.as_json()}, status=400)
+
+@api_view(['POST'])
+def book_property(request, pk):
+    try:
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        number_of_nights = request.POST.get('number_of_nights', '')
+        total_price = request.POST.get('total_price', '')
+        guests = request.POST.get('guests', '')
+
+        property = Property.objects.get(pk=pk)
+
+        Reservation.objects.create(
+            property=property,
+            start_date=start_date,
+            end_date=end_date,
+            number_of_nights=number_of_nights,
+            total_price=total_price,
+            guests=guests,
+            created_by=request.user
+        )
+
+        return JsonResponse({'success': True})
+    except Exception as e:
+        print('Error', e)
+
+        return JsonResponse({'success': False})
--- backend/djangobnb_backend/property/urls.py
+++ backend/djangobnb_backend/property/urls.py
@@ -5,5 +5,6 @@ from . import api
 urlpatterns = [
     path('', api.properties_list, name='api_properties_list'),
     path('create/', api.create_property, name='api_create_property'),
-    path('<uuid:pk>', api.properties_detail, name='api_properties_detail'),
+    path('<uuid:pk>/', api.properties_detail, name='api_properties_detail'),
+    path('<uuid:pk>/book/', api.book_property, name='api_book_property'),
 ]
